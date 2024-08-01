d7661360b89fdd4d0f6b47addeedde74a7dbf383
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-01 11:31:18 -0300

    Adicionando rotas

--- web/src/AppRoutes.tsx
+++ web/src/AppRoutes.tsx
@@ -2,18 +2,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/pages/Dashboard";
 import PaginaBase from "./components/pages/PaginaBase";
 import PaginaInicial from "./components/pages/PaginaInicial";
+import PaginaBaseFormulario from "./components/pages/PaginaBaseFromulario";
+import Login from "./components/pages/Login";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PaginaBase />} >
-        <Route index element={<PaginaInicial />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<PaginaInicial />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/" element={<PaginaBaseFormulario/>}>
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default AppRoutes
+export default AppRoutes;
